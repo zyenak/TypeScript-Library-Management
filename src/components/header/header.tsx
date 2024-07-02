452538f0dd969c7c71c8b42fa8027d815c7d0138
zyenak <zyenakamranafzal@gmail.com>
2024-07-02 16:44:08 +0500

    router v6.3 used

---
+++ src/components/header/header.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import AdbIcon from "@mui/icons-material/Adb";
+import { useUser } from "../../context/user-context";
+
+const AppHeader: React.FC<{ onLoginClick: () => void }> = ({ onLoginClick }) => {
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const { user, logoutUser } = useUser();
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleLogout = () => {
+    logoutUser();
+    handleCloseUserMenu();
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: "flex", mr: 1 }} />
+          <Link to="/" style={{ textDecoration: "none", flexGrow: 1 }}>
+            <Typography
+              variant="h6"
+              noWrap
+              sx={{
+                mr: 2,
+                display: "flex",
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "white",
+              }}
+            >
+              Library Management System
+            </Typography>
+          </Link>
+          <Box sx={{ flexGrow: 0 }}>
+            {user ? (
+              <>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar>{user.username.charAt(0).toUpperCase()}</Avatar>
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">Dashboard</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleLogout}>
+                    <Typography textAlign="center">Logout</Typography>
+                  </MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <Button
+                onClick={onLoginClick}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Login
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default AppHeader;
--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -1,20 +1,6 @@
 import React, { useState, useContext } from "react";
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Menu,
-  Container,
-  Avatar,
-  Button,
-  Tooltip,
-  MenuItem,
-} from "@mui/material";
 import { useUser } from "../context/user-context";
 import { Route, Routes, Navigate, Link, useNavigate } from "react-router-dom";
-import AdbIcon from "@mui/icons-material/Adb";
 import BooksList from "../containers/dashboard";
 import { LoginDialog } from "../components/login/login-dialog";
 import { BooksContext } from "../context/books-context";
@@ -22,21 +8,14 @@ import CustomForm, { FormField } from "../components/forms/custom-form";
 import WithAdminProtector from "../middleware/admin-protector";
 import { WithLoginProtector } from "../middleware/login-protector";
 import withInputValidation from '../hoc/input-error-handling';
+import AppHeader from "../components/header/header";
 
 const AppLayout: React.FC = () => {
   const [openLoginDialog, setOpenLoginDialog] = useState(false);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  const { user, loginUser, logoutUser, addUser } = useUser();
+  const { user, loginUser, addUser } = useUser();
   const { addBook, updateBook } = useContext(BooksContext);
   const navigate = useNavigate();
 
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   const handleLoginSubmit = (username: string, password: string) => {
     loginUser(username, password);
@@ -47,10 +26,6 @@ const AppLayout: React.FC = () => {
     setOpenLoginDialog(false);
   };
 
-  const handleLogout = () => {
-    logoutUser();
-    handleCloseUserMenu();
-  };
 
   const handleUserSubmit = (data: any, toUpdate: boolean) => {
     addUser(data);
@@ -98,138 +73,81 @@ const AppLayout: React.FC = () => {
   const ValidatedBookForm = withInputValidation(CustomForm);
   const ValidatedUserForm = withInputValidation(CustomForm);
 
+
+
+  const publicRoutes = [
+    { path: "/books", element: <BooksList /> },
+  ];
+
+  const privateRoutes = [
+    {
+      path: "/admin/books/add",
+      element: (
+        <WithLoginProtector>
+          <WithAdminProtector>
+            <ValidatedBookForm
+              formType="book"
+              initialData={initialBookState}
+              toUpdate={false}
+              onSubmit={handleBookSubmit}
+              fields={bookFields}
+              validateForm={function (formData: any, fields: FormField[]): boolean {
+                throw new Error("Function not implemented.");
+              }} />
+          </WithAdminProtector>
+        </WithLoginProtector>
+      ),
+    },
+    {
+      path: "/admin/users/add",
+      element: (
+        <WithLoginProtector>
+          <WithAdminProtector>
+            <ValidatedUserForm
+              formType="user"
+              initialData={initialUserState}
+              toUpdate={false}
+              onSubmit={handleUserSubmit}
+              fields={userFields}
+              validateForm={function (formData: any, fields: FormField[]): boolean {
+                throw new Error("Function not implemented.");
+              }} />
+          </WithAdminProtector>
+        </WithLoginProtector>
+      ),
+    },
+    {
+      path: "/admin/books/:bookIsbn/edit",
+      element: (
+        <WithLoginProtector>
+          <WithAdminProtector>
+            <ValidatedBookForm
+              formType="book"
+              initialData={initialBookState}
+              toUpdate={true}
+              onSubmit={handleBookSubmit}
+              fields={bookFields}
+              validateForm={function (formData: any, fields: FormField[]): boolean {
+                throw new Error("Function not implemented.");
+              }} />
+          </WithAdminProtector>
+        </WithLoginProtector>
+      ),
+    },
+  ];
+
   return (
     <>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <AdbIcon sx={{ display: "flex", mr: 1 }} />
-            <Link to="/" style={{ textDecoration: "none", flexGrow: 1 }}>
-              <Typography
-                variant="h6"
-                noWrap
-                sx={{
-                  mr: 2,
-                  display: "flex",
-                  fontFamily: "monospace",
-                  fontWeight: 700,
-                  letterSpacing: ".3rem",
-                  color: "white",
-                }}
-              >
-                Library Management System
-              </Typography>
-            </Link>
-            <Box
-              sx={{
-                flexGrow: 0,
-              }}
-            >
-              {user ? (
-                <>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar>
-                        {user.username.charAt(0).toUpperCase()}
-                      </Avatar>
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                  >
-                    <MenuItem onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center">Dashboard</Typography>
-                    </MenuItem>
-                    <MenuItem onClick={handleLogout}>
-                      <Typography textAlign="center">Logout</Typography>
-                    </MenuItem>
-                  </Menu>
-                </>
-              ) : (
-                <Button
-                  onClick={() => {
-                    setOpenLoginDialog(true);
-                  }}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  Login
-                </Button>
-              )}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+      <AppHeader onLoginClick={() => setOpenLoginDialog(true)} />
       <Routes>
-        <Route path="/books" element={<BooksList />} />
-        <Route
-          path="/admin/books/add"
-          element={
-            <WithLoginProtector>
-              <WithAdminProtector>
-                <ValidatedBookForm
-                  formType="book"
-                  initialData={initialBookState}
-                  toUpdate={false}
-                  onSubmit={handleBookSubmit}
-                  fields={bookFields} 
-                  validateForm={function (formData: any, fields: FormField[]): boolean {
-                    throw new Error("Function not implemented.");
-                  } }              />
-              </WithAdminProtector>
-            </WithLoginProtector>
-          }
-        />
-        <Route
-          path="/admin/users/add"
-          element={
-            <WithLoginProtector>
-              <WithAdminProtector>
-                <ValidatedUserForm
-                  formType="user"
-                  initialData={initialUserState}
-                  toUpdate={false}
-                  onSubmit={handleUserSubmit}
-                  fields={userFields} 
-                  validateForm={function (formData: any, fields: FormField[]): boolean {
-                    throw new Error("Function not implemented.");
-                  } }              />
-              </WithAdminProtector>
-            </WithLoginProtector>
-          }
-        />
-        <Route
-          path="/admin/books/:bookIsbn/edit"
-          element={
-            <WithLoginProtector>
-              <WithAdminProtector>
-                <ValidatedBookForm
-                  formType="book"
-                  initialData={initialBookState}
-                  toUpdate={true}
-                  onSubmit={handleBookSubmit}
-                  fields={bookFields} 
-                  validateForm={function (formData: any, fields: FormField[]): boolean {
-                    throw new Error("Function not implemented.");
-                  } }               />
-              </WithAdminProtector>
-            </WithLoginProtector>
-          }
-        />
-        <Route path="*" element={<Navigate to="/books" replace />} />
-      </Routes>
+      {publicRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+      {privateRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+      <Route path="*" element={<Navigate to="/books" replace />} />
+    </Routes>
       <LoginDialog
         open={openLoginDialog}
         handleSubmit={handleLoginSubmit}
